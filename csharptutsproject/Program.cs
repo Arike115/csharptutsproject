322dc26c611fcf9b6267e51ab22d33df83dbfada
Arike115 <ajibolaoluwafunke669@gmail.com>
2022-08-25 18:16:59 +0100

    Add project files.

---
+++ csharptutsproject/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace csharptutsproject
+{
+
+    class Program
+    {
+
+        //Assignment Operator =
+        //Arithmetic operators +,-,*,/,%
+        //comparison operators == -> equalto, != -> notequalto, >, >=,<,<=
+        //Conditional Operators &&,||
+        //Ternary Operator ?
+        public static void Main()
+        {
+            int b = 10;
+            int c = 45;
+
+            bool isb10 = b == 10 ? true:false;
+
+            int d = b == 10 ? 15 : b;
+
+            if(b == 10 || c == 7)
+            {
+                Console.WriteLine("they are the CORRECT");
+            }
+            else
+            {
+                Console.WriteLine("they are not");
+            }
+
+            int g = b / c;
+
+            Console.WriteLine(d);   
+
+            Multiplication(24, 67);
+           
+        }
+
+        public static void Multiplication(int fig, int figs)
+        { 
+        }
+
+    }
+
+
+}
+
---
+++ csharptutsproject/StarterClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharptutsproject
+{
+    public class StarterClass
+    {
+        public static void mycode()
+        {
+            Console.WriteLine("Hello World");
+
+
+            
+        }
+    }
+}
