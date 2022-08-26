6619374e42cc65e3c981fcda72040e0099b7fe49
Arike115 <ajibolaoluwafunke669@gmail.com>
2022-08-26 19:17:02 +0100

    changes

--- csharptutsproject/Program.cs
+++ csharptutsproject/Program.cs
@@ -1,60 +1,14 @@
 ﻿namespace csharptutsproject
 {
-
+    
     class Program
     {
-        //statements
-        //conditional statement
+        
+        
         public static void Main()
         {
-            int TotalAmount = 0;
-
-            Start:
-            Console.WriteLine("select your choice of bread; 1 .. smallsize, 2.. mediumsize, 3... largesize");
-            int userchoice = int.Parse(Console.ReadLine());
-
-            switch (userchoice)
-            {
-                case 1:
-                    TotalAmount += 100;
-                    break;
-                case 2:
-                    TotalAmount += 200;
-                    break;
-                case 3:
-                    TotalAmount += 400;
-                    break;
-                    default:
-                    Console.WriteLine("your choice {0}is invalid", userchoice);
-                    goto Start;
-
-
-            }
-
-            confirm:
-
-            Console.WriteLine("Do you want another bread - Yes or No");
-            string userdecision = Console.ReadLine();
-
-            switch (userdecision.ToUpper())
-            {
-                case "Yes":
-                    goto Start;
-                case "No":
-                    break;
-                default:
-                    Console.WriteLine("your choice {0}is invalid", userdecision);
-                    goto confirm;
-            }
-
-
-            Console.WriteLine("Thank you for shopping with us");
-            Console.WriteLine("Bill Amount = {0}", TotalAmount);
-
-
-
-
-
+            StarterClass start = new StarterClass();
+            start.Callmethod();
         }
 
 
--- csharptutsproject/StarterClass.cs
+++ csharptutsproject/StarterClass.cs
@@ -6,15 +6,33 @@ using System.Threading.Tasks;
 
 namespace csharptutsproject
 {
+    //class
+    //properties or field ------- data
+    //constructor ----- representative of class
+    //method/functions -------- behaviour
+
     public class StarterClass
     {
-        public static void mycode()
+        string Name = "Ben";
+        int Age = 25;
+        DateTime CreatedOn = DateTime.Now;
+
+        //contructor
+        //public StarterClass(string name, int age)
+        //{
+        //    Name = name;
+        //    Age = age;  
+        //}
+
+        public void Callmethod()
         {
-            
-            Console.WriteLine("Hello World");
-
-
-            
+            Console.WriteLine("my name is {0}, am {1} years old", Name, Age);
+            Console.WriteLine("The time is {0}", CreatedOn);
         }
+
+
+
+
+
     }
 }
