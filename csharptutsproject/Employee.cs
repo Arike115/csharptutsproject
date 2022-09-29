60d895feeea803970bd465e68eda156ef7f2bb08
Arike115 <ajibolaoluwafunke669@gmail.com>
2022-09-29 17:15:32 +0100

    changes

---
+++ csharptutsproject/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharptutsproject
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int AnnualSalary { get; set; }
+
+
+        public static List<Employee> Getallemployee()
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee { EmployeeId = 101, FirstName = "Ben", LastName = "Roy", Gender = "Male", AnnualSalary = 100000 });
+            employees.Add(new Employee { EmployeeId = 102, FirstName = "Jude", LastName = "sam", Gender = "Male", AnnualSalary = 500000 });
+            employees.Add(new Employee { EmployeeId = 103, FirstName = "Tom", LastName = "Fred", Gender = "Male", AnnualSalary = 50000 });
+            employees.Add(new Employee { EmployeeId = 104, FirstName = "Richy", LastName = "Smith", Gender = "Male", AnnualSalary = 700000 });
+            employees.Add(new Employee { EmployeeId = 105, FirstName = "Joy", LastName = "Lawn", Gender = "Female", AnnualSalary = 300000 });
+            employees.Add(new Employee { EmployeeId = 106, FirstName = "Carly", LastName = "Vicky", Gender = "Female", AnnualSalary = 70000 });
+            employees.Add(new Employee { EmployeeId = 107, FirstName = "Lyla", LastName = "Matt", Gender = "Female", AnnualSalary = 400000 });
+
+            return employees;
+
+
+        }
+    }
+}
--- csharptutsproject/Program.cs
+++ csharptutsproject/Program.cs
@@ -3,82 +3,37 @@
 namespace csharptutsproject
 
 {
-    //IEnumerable
-
     public class Program
     {
 
-        //stack === LIFO ----- LastIN FirstOut
-        //queue === FIFO ----- FirstIn FirstOut
-        //Dictionary === is a key value pair
-
-
+       //Agregate operators
+       //Join Operators 
         public static void Main()
         {
-            Dictionary<int, string> data = new Dictionary<int, string>();
+            
+            
+           var empl = Employee.Getallemployee().Select(emp => new 
+           {
+               FullName = emp.FirstName + ' '+emp.LastName, monthlYSalary = emp.AnnualSalary/12
+           });
 
-            data.Add(1, "one");
-            data.Add(2, "two");
-            data.Add(45, "fortyfive");
-
-            foreach(KeyValuePair<int,string> kvp in data)
+            foreach(var em in empl)
             {
-                Console.WriteLine(kvp.Value);
+                Console.WriteLine("fullname----{0} ----- MonthlySalary ------{1}", em.FullName,em.monthlYSalary);
             }
 
 
-
-            HashSet<int> set = new HashSet<int>();
-            set.Add(1);
-            set.Add(2);
-            set.Add(3);
-            set.Add(4);
-            set.Add(1);
-            set.Add(1);
-            set.Add(1);
-
-            foreach (int i in set)
+            var emx = Employee.Getallemployee().SelectMany(ems => ems.Gender.ToCharArray());
+            foreach(char em in emx)
             {
-                Console.WriteLine(i);
+                
+                Console.WriteLine(em);
             }
 
-            //string[] words = { "joy", "word", "doll" };
+          // Console.ReadKey();
+        } 
 
-            //Stack<int> stack = new Stack<int>();
-            //stack.Push(1);
-            //stack.Push(2);
-            //stack.Push(3);
-
-
-            //Console.WriteLine(stack.Peek());
-            //stack.Pop();
-            //Console.WriteLine(stack.Peek());
-            //stack.Pop();
-            //Console.WriteLine(stack.Peek());
-
-            //Queue<string> queue = new Queue<string>();
-            //queue.Enqueue("word");
-            //queue.Enqueue("chalk");
-            //queue.Enqueue("top");
-            //queue.Enqueue("left");
-            //Console.WriteLine(queue.Peek());
-            ////queue.Dequeue();
-            //Console.WriteLine(queue.Peek());
-            ////queue.Dequeue();
-            //Console.WriteLine(queue.Peek());
-            ////queue.Dequeue();
-            //Console.WriteLine(queue.Peek());
-
-            //List<int> list = stack.ToList();
-            //list.Add(list[0]);
-            //list.Add(list[1]);
-            //list.Add(list[3]);
-
-
-
-        }
         
-      
 
 
 
